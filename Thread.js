3e70364a9c2316720c8e238f944ed64f3d4d19f8
gpgkd906 <gpgkd906@gmail.com>
2016-04-14 18:27:23 +0900

    add worker pool

--- Thread.js
+++ Thread.js
@@ -24,9 +24,40 @@
     let message_id_incrementer = 0;
     const WorkerMap = new Map();
     const PromiseMap = new Map();
+    const FuncMap = new Map();    
     let createWorker = null;
+    let createFunc = null;
     if (exports.Worker && exports.URL) {
 	useWorker = true;
+	exports.Worker.prototype.lockStatus = false;
+	exports.Worker.prototype.isLocked = function () {
+	    return this.lockStatus;
+	};
+	exports.Worker.prototype.lock = function () {
+	    this.lockStatus = true;
+	    return true;
+	};
+	exports.Worker.prototype.unlock = function () {
+	    this.lockStatus = false;
+	    return true;
+	};
+	let cores =  navigator.hardwareConcurrency;
+	WorkerMap.setWorker = (key, worker) => {
+	    let workerPool = WorkerMap.get(key) || [];
+	    if(workerPool.length < cores) {
+		workerPool.push(worker);
+	    }
+	    WorkerMap.set(key, workerPool);
+	};
+	WorkerMap.getFreeWorker = key => {
+	    let workerPool = WorkerMap.get(key) || [];
+	    for(let worker of workerPool) {
+		if(worker.isLocked() === false) {
+		    return worker;
+		}
+	    }
+	    return null;
+	};
 	const thread_onmessage = function onmessage(event) {
 	    "use strict";
 	    const move = [];
@@ -44,7 +75,7 @@
 	};
 	createWorker = fn => {
 	    let fnStr = fn.toString();
-	    if(!WorkerMap.has(fnStr)) {
+	    if(!WorkerMap.getFreeWorker(fnStr)) {
 		let worker = new exports.Worker(
 		    exports.URL.createObjectURL(
 			new Blob([
@@ -56,15 +87,28 @@
 		    let message_id = event.data.message_id;
 		    PromiseMap.get(message_id).resolve(event.data.result);
 		    PromiseMap.delete(message_id);
+		    worker.unlock();
 		};
 		worker.onerror = error => {
 		    PromiseMap.get(message_id).reject(error);
 		    PromiseMap.delete(message_id);
+		    worker.unlock();
 		};
-		WorkerMap.set(fnStr, worker);
+		WorkerMap.setWorker(fnStr, worker);
+		return worker;
 	    }
-	    return WorkerMap.get(fnStr);
+	    return WorkerMap.getFreeWorker(fnStr);
 	};
+    } else {
+	createFunc = fn => {
+	    let fnStr = fn.toString();
+	    if(!FuncMap.has(fnStr)) {
+		let newFn = new Function("data", '"use strict"; return (' + fnStr + ').call(null, data, transferable => transferable);');
+		FuncMap.set(fnStr, newFn);
+	    }
+	    return FuncMap.get(fnStr);
+	}
+	
     }
     exports.Thread = {
 	spawn: function({
@@ -77,6 +121,7 @@
 		return new Promise((resolve, reject) => {
 		    PromiseMap.set(message_id, {resolve, reject});
 		    let worker = createWorker(fn);
+		    worker.lock();
 		    if(!!move) {
 			worker.postMessage({data, message_id}, move);
 		    } else {
@@ -86,7 +131,7 @@
 	    } else {
 		return new Promise((resolve, reject) => {
 		    try {
-			resolve(fn.call(null, data, dummy => dummy));
+			resolve(createFunc(fn).call(null, data));
 		    } catch(error) {
 			reject(error);
 		    }
