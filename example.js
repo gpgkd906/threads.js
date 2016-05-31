71f64678d6a20105daf6762e7f1c73ddc3c5687a
han chen <han chen>
2016-05-31 23:49:31 +0900

    fix indent

--- example.js
+++ example.js
@@ -1,20 +1,20 @@
 "use strict";
 if(!Thread) {
-    var Thread = require("./lib/Thread.js").Thread;
+  var Thread = require("./lib/Thread.js").Thread;
 }
 let count = 10;
 let startTime = Date.now();
 for(let i = 0; i < count; i++) {
-    let data = new Uint8Array(100*1024*1024);
-    Thread.spawn({
-	data: data,
-	move: [data.buffer],
-	fn: function(data, move) {
-            move(data.buffer);
+  let data = new Uint8Array(100*1024*1024);
+  Thread.spawn({
+	  data: data,
+	  move: [data.buffer],
+	  fn: function(data, move) {
+      move(data.buffer);
 	    return data;
-	}
-    }).then(function(data) {
-	console.log((Date.now() - startTime) + " ms");
-    });
+	  }
+  }).then(function(data) {
+	  console.log((Date.now() - startTime) + " ms");
+  });
 }
 
